29ac83ae2b59dfbdc72eb18b646dd74417d753ff
Apostolis <Apostoliszax@hotmail.com>
2025-10-10 13:37:30 +0300

    fix

--- model.py
+++ model.py
@@ -1,7 +1,5 @@
 import torch.nn as nn
 
-#TODO refactor
- 
 class NeuralNetwork(nn.Module):
     """
     A fully connected neural network with three hidden layers and dropout regularization.
@@ -14,28 +12,28 @@ class NeuralNetwork(nn.Module):
     """
     def __init__(self, inputSize, hiddenSize, numClasses, dropout=0.3):
         super(NeuralNetwork, self).__init__()
-        self.fullyConnectedLayer1 = nn.Linear(inputSize, hiddenSize)
-        self.activationFunction1 = nn.ReLU()
+        self.fc1 = nn.Linear(inputSize, hiddenSize)
+        self.act1 = nn.ReLU()
         self.dropout1 = nn.Dropout(dropout)
 
-        self.fullyConnectedLayer2 = nn.Linear(hiddenSize, hiddenSize)
-        self.activationFunction2 = nn.ReLU()
+        self.fc2 = nn.Linear(hiddenSize, hiddenSize)
+        self.act2 = nn.ReLU()
         self.dropout2 = nn.Dropout(dropout)
 
-        self.fullyConnectedLayer3 = nn.Linear(hiddenSize, hiddenSize)
-        self.activationFunction3 = nn.ReLU()
+        self.fc3 = nn.Linear(hiddenSize, hiddenSize)
+        self.act3 = nn.ReLU()
         self.dropout3 = nn.Dropout(dropout)
 
         self.out = nn.Linear(hiddenSize, numClasses)
 
     def forward(self, x):
-        x = self.activationFunction1(self.fullyConnectedLayer1(x))
+        x = self.act1(self.fc1(x))
         x = self.dropout1(x)
 
-        x = self.activationFunction2(self.fullyConnectedLayer2(x))
+        x = self.act2(self.fc2(x))
         x = self.dropout2(x)
 
-        x = self.activationFunction3(self.fullyConnectedLayer3(x))
+        x = self.act3(self.fc3(x))
         x = self.dropout3(x)
 
         x = self.out(x)
